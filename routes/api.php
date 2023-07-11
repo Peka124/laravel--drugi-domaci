60d1c5e46426bbf4d887932fb18da1c314050985
Djordje Tornjanski <djordjepeka124@gmail.com>
2023-07-12 00:14:09 +0200

    Ispravka 2

--- routes/api.php
+++ routes/api.php
@@ -36,7 +36,7 @@ Route::post('login', [AutfKontroler::class, 'login']);
 
 
 
-Route::group(['middlware'=>['auth:sanctum']], function() {
+Route::group(['middleware'=>['auth:sanctum']], function() {
     Route::resource('forum', ForumKontroler::class)->only('index', 'show', 'destroy', 'update');
     Route::resource('clan', ClanKontroler::class)->only('index', 'show');
     Route::post('logout', [AutfKontroler::class, 'logout']);
